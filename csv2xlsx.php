bab15434b5a1ebe7d92348d3daee93b4122b47cd
Artūrs Jansons <arturs.jansons05@latvenergo.lv>
2015-09-15 11:48:19 +0300

    Add csv2xlsx.php

---
+++ csv2xlsx.php
@@ -0,0 +1,33 @@
+<?php
+
+error_reporting(E_ALL);
+date_default_timezone_set('Europe/London');
+
+/** PHPExcel_IOFactory */
+
+require_once dirname(__FILE__) . DIRECTORY_SEPARATOR.'Classes'.DIRECTORY_SEPARATOR.'PHPExcel'.DIRECTORY_SEPARATOR.'IOFactory.php';
+
+$filename = '';
+foreach ($argv as $key=>$value){
+	preg_match('/csv/',$value,$matches, PREG_OFFSET_CAPTURE);
+	if(count($matches))
+		$filename = $value;
+};
+
+// Check prerequisites
+
+if (!file_exists($filename)) {
+    exit("File not found\n");
+}
+
+$objPHPExcel = PHPExcel_IOFactory::load($filename);
+$objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel,'Excel5');
+/*
+$objWriter->setInputEncoding('UTF8');
+$objWriter->setUseBOM(false);
+$objWriter->setDelimiter(',');
+$objWriter->setEnclosure('"');
+$objWriter->setLineEnding("\r\n");
+*/
+$objWriter->save(str_replace('.csv', '.xlsx',$filename));
+?>
